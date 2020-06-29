18c32ca0d0eb5b51f75870a6d986bc1a4bdecefc
Max Kornyev <mkornyev@novetta.com>
2020-06-28 22:21:49 -0400

    Style changes, mobile bug fixed

--- mkv2/forms.py
+++ mkv2/forms.py
@@ -4,9 +4,27 @@ from django.db import models
 from mkv2.models import *
 import django_filters
 
+INPUT_ATTRIBUTES = {'class' : 'form-input form-control'}
+
 class ProjectFilter(django_filters.FilterSet):
 	tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple, label='')
 
 	class Meta:
 		model = Project
 		fields = ('tags',)
+
+class LoginForm(forms.Form):
+	username = forms.CharField(max_length = 50, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
+	password = forms.CharField(max_length = 50, widget=forms.PasswordInput(attrs=INPUT_ATTRIBUTES))
+
+	def clean(self):
+		cleaned_data = super().clean()
+		username = cleaned_data.get('username')
+		password = cleaned_data.get('password')
+
+		user = authenticate(username=username, password=password)
+
+		if not user:
+			raise forms.ValidationError("Invalid Username or Password Entered")
+
+		return cleaned_data
