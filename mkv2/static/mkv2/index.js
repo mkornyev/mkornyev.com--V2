bebf023c6764111e8e5d15f852caf78e31c4255d
mkornyev <mkornyev@gmail.com>
2021-05-21 20:29:59 -0400

    UI and Asset Updates:
    - new resume added
    - carousel image added
    - removed scrolltop callback
    - mobile styling fixes

--- mkv2/static/mkv2/index.js
+++ mkv2/static/mkv2/index.js
@@ -1,5 +1,8 @@
 
 $(document).ready(function (){
   // Reset scroll position before reload
-  $(window).on('beforeunload', function() { $(window).scrollTop(0) })
+  // $(window).on('beforeunload', function(event) { 
+  //   if( $(event.target).is($('#nav3').children().first()) ) return 
+  //   $(window).scrollTop(0) 
+  // })
 })
